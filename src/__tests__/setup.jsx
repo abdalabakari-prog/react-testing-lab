bbaffe7d534807af43fe0e0077fd82bfbc9fbc47
abdalabakari-prog <abdala.bakari@student.moringaschool.com>
2026-01-19 21:08:52 +0300

    Update setup.jsx

--- src/__tests__/setup.jsx
+++ src/__tests__/setup.jsx
@@ -1,17 +1,19 @@
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-import '@testing-library/jest-dom/vitest'
-import fetch from 'node-fetch';
+import { afterEach, vi } from "vitest";
+import { cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
 
-  
 global.setFetchResponse = (val) => {
-    global.fetch = vi.fn(() => Promise.resolve({
-        json: () => Promise.resolve(val),
-        ok: true,
-        status: 200
-    }))
-}
+  global.fetch = vi.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(val),
+      ok: true,
+      status: 200,
+    })
+  );
+};
 
 afterEach(() => {
-    cleanup();
-})
+  cleanup();
+});
+
+
